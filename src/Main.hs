47ca4013b558924bc5557a3b19ed4e79a52d918c
Norbert Melzer <timmelzer@gmail.com>
2014-12-10 17:23:00 +0100

    make tests pass

--- src/Main.hs
+++ src/Main.hs
@@ -2,66 +2,39 @@
 
 module Main where
 
-import Control.Applicative((<*))
-import Text.Parsec
-import Text.Parsec.String
-import Text.Parsec.Expr
-import Text.Parsec.Token
-import Text.Parsec.Language
+import Control.Monad
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Char
+import System.Environment
 
-data Expr = Var String
-          | Con Bool
-          | Uno Unop Expr
-          | Duo Duop Expr Expr
-          deriving (Show)
+parseOctNum :: Parser String
+parseOctNum = do
+  string "0" <|> string "o" <|> string "O"
+  octStr <- many1 octDigit
+  return ('0':'o':octStr)
 
-data Unop = Not
-          deriving (Show)
+parseHexNum :: Parser String
+parseHexNum = do
+  try (string "0x") <|> string "0X"
+  hexStr <- many1 hexDigit
+  return ('0':'x':hexStr)
 
-data Duop = And
-          | Iff
-          deriving (Show)
+parseDecNum :: Parser String
+parseDecNum = do
+  decStr <- many1 digit
+  return decStr
 
-data Stmt = Nop
-          | String := Expr
-          | If Expr Stmt Stmt
-          | While Expr Stmt
-          | Seq [Stmt]
-          deriving (Show)
-
-def = emptyDef { commentStart    = "{-"
-               , commentEnd      = "-}"
-               , identStart      = letter
-               , identLetter     = alphaNum
-               , opStart         = oneOf "~&=:"
-               , opLetter        = oneOf "~&=:"
-               , reservedOpNames = ["~", "&", "=", ":="]
-               , reservedNames   = ["true", "false",
-                                    "nop",
-                                    "if", "then", "else", "fi",
-                                    "while", "do", "od"]
-               }
-
-TokenParser { parens     = m_parens
-            , identifier = m_identifier
-            , reservedOp = m_reservedOp
-            , reserved   = m_reserved
-            , semiSep1   = m_semiSep1
-            , whiteSpace = m_whiteSpace } = makeTokenParser def
-
-exprparser :: Parser Expr
-exprparser = buildExpressionParser table term <?> "expression"
-
-table = [ [Prefix (m_reservedOp "~" >> return (Uno Not))]
-        , [Infix  (m_reservedOp "&" >> return (Duo And)) AssocLeft]
-        , [Infix  (m_reservedOp "=" >> return (Duo Iff)) AssocLeft]
-        ]
-
-term = m_parens exprparser
-       <|> fmap Var m_identifier
-       <|> (m_reserved "true"  >> return (Con True))
-       <|> (m_reserved "false" >> return (Con False))
+parseInt :: Parser Integer
+parseInt = do
+  numStr <- try parseOctNum <|>
+            try parseHexNum <|>
+            parseDecNum
+  return $ read numStr
 
 main :: IO ()
-main = putStrLn "It works"
-
+main = do
+  source <- liftM head getArgs
+  print source
+  case parse parseInt "Commandline Argument Testparser" source of
+    Left  err -> print err
+    Right res -> print res
