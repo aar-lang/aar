a74388bcee8cf5d3cd9c81263c84512497b6e11f
Norbert Melzer <timmelzer@gmail.com>
2015-01-11 17:58:00 +0100

    AST-Fields are strict now

--- lib/Aar/AST.hs
+++ lib/Aar/AST.hs
@@ -1,5 +1,5 @@
 module Aar.AST where
 
 -- | The Abstract Syntax Tree
-data AST = Import  String -- ^ Importing a module
-         | FunCall String [AST] -- ^ Call the function with its arguments
+data AST = Import  !String -- ^ Importing a module
+         | FunCall !String ![AST] -- ^ Call the function with its arguments
