f9bd0a8ad5a6130a272acb3981c794c49fc2cce9
Norbert Melzer <timmelzer@gmail.com>
2015-01-15 13:37:14 +0100

    very little documentation

--- lib/Aar/AST.hs
+++ lib/Aar/AST.hs
@@ -1,5 +1,6 @@
 module Aar.AST where
 
 -- | The Abstract Syntax Tree
-data AST = Import  !String -- ^ Importing a module
+data AST = Import  !String !(Maybe String) -- ^ Importing the module specified by the first `String`, it is always fully
+                                           --   qualified. The `(Maybe String)` does save an optional qualification alias.
          | FunCall !String ![AST] -- ^ Call the function with its arguments
